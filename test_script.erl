cebcafbd523d6e797fcf55c29a2238359b322dd9
Marc Neudert <marc.neudert@gmail.com>
2015-04-22 20:06:23 +0200

    connects all started nodes

--- test_script.erl
+++ test_script.erl
@@ -1,6 +1,18 @@
 #!/usr/bin/env escript
 
 main(_) ->
-    net_kernel:start([ multinode, shortnames ]),
-    io:fwrite("local node: ~w~n", [ node() ]),
+    startup(),
+    connect_nodes(),
     ok.
+
+
+startup() ->
+    io:fwrite("start main node: ~w~n", [ net_kernel:start([ multinode, shortnames ]) ]),
+    io:fwrite("local node: ~w~n", [ node() ]),
+    io:fwrite("~n").
+
+connect_nodes() ->
+    io:fwrite("ping 'detached@localhost': ~w~n", [ net_adm:ping('detached@localhost') ]),
+    io:fwrite("ping 'rpckill@localhost': ~w~n", [ net_adm:ping('rpckill@localhost') ]),
+    io:fwrite("connected nodes: ~w~n", [ nodes() ]),
+    io:fwrite("~n").
