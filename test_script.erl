170a8f0a23ec842970283f17cce0b0e01e417895
Marc Neudert <marc.neudert@gmail.com>
2015-04-23 20:20:25 +0200

    kills a node using rpc:call/4

--- test_script.erl
+++ test_script.erl
@@ -3,6 +3,7 @@
 main(_) ->
     startup(),
     connect_nodes(),
+    try_rpckill(),
     ok.
 
 
@@ -14,5 +15,11 @@ startup() ->
 connect_nodes() ->
     io:fwrite("ping 'detached@localhost': ~w~n", [ net_adm:ping('detached@localhost') ]),
     io:fwrite("ping 'rpckill@localhost': ~w~n", [ net_adm:ping('rpckill@localhost') ]),
+    io:fwrite("connected nodes: ~w~n", [ nodes() ]),
+    io:fwrite("~n").
+
+try_rpckill() ->
+    io:fwrite("kill node via rpc: ~w~n", [ rpc:call('rpckill@localhost', init, stop, []) ]),
+    timer:sleep(1000),
     io:fwrite("connected nodes: ~w~n", [ nodes() ]),
     io:fwrite("~n").
